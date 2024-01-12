249c724631b2507bfe0b91087251a785107bcdce
benjamin <blefrancois@viapresse.com>
2024-01-12 16:42:34 +0100

    applyFilter method completed,add condition in didSet of toDoItems to avoid losing items when filter is applied

--- ToDoList/Sources/ViewModel/ToDoListViewModel.swift
+++ ToDoList/Sources/ViewModel/ToDoListViewModel.swift
@@ -4,6 +4,7 @@ final class ToDoListViewModel: ObservableObject {
     // MARK: - Private properties
 
     private let repository: ToDoListRepositoryType
+    private var filterApplied = false
 
     // MARK: - Init
 
@@ -17,7 +18,9 @@ final class ToDoListViewModel: ObservableObject {
     /// Publisher for the list of to-do items.
     @Published var toDoItems: [ToDoItem] = [] {
         didSet {
-            repository.saveToDoItems(toDoItems)
+            if !filterApplied {
+                repository.saveToDoItems(toDoItems)
+            }
         }
     }
 
@@ -43,5 +46,18 @@ final class ToDoListViewModel: ObservableObject {
     /// Apply the filter to update the list.
     func applyFilter(at index: Int) {
         // TODO: - Implement the logic for filtering
+        // update filterApplied value
+        filterApplied = index != 0
+        
+        switch index {
+        case 0:
+            toDoItems = repository.loadToDoItems()
+        case 1:
+            toDoItems = repository.loadToDoItems().filter({ $0.isDone == true })
+        case 2:
+            toDoItems = repository.loadToDoItems().filter({ $0.isDone == false })
+        default:
+            break
+        }
     }
 }
